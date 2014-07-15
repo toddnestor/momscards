9a6688eed01008884945b77257b4b8f2d250ab2d
Todd Nestor <todd.nestor@gmail.com>
2014-07-16 02:25:19 +0800

    basic template up, wording wrong

---
+++ inc/footer.php
@@ -0,0 +1,19 @@
+	</div>
+
+	<div class="footer">
+
+		<div class="wrapper">
+
+			<ul>		
+				<li><a href="http://twitter.com/sherriscards">Twitter</a></li>
+				<li><a href="https://www.facebook.com/sherriscards">Facebook</a></li>
+			</ul>
+
+			<p>&copy;<?php echo date('Y'); ?> Sherri Nestor</p>
+
+		</div>
+	
+	</div>
+
+</body>
+</html>
